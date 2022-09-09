ab03051369f498417c9bca459fa6782e71c22869
Dhanya-2408 <dhanya240898@example.com>
2022-09-09 16:16:08 +0530

    first commit

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,6 @@
 import { Banner } from "../../components/Banner";
 import { SiteFeatures } from "../../components/SiteFeatures";
 import { SiteInfo } from "../../components/SiteInfo";
-import { Navbar } from "../Navbar";
 
 export const Home = () => {
   return (
