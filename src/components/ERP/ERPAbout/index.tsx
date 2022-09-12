940cf51004d522c4a4597f333e4d3f23fe4d4268
Dhanya-2408 <dhanya240898@example.com>
2022-09-12 15:07:37 +0530

    ERP About

--- src/components/ERP/ERPAbout/index.tsx
+++ src/components/ERP/ERPAbout/index.tsx
@@ -1,10 +1,68 @@
 import React from "react";
+import LazyLoad from "../../../ui-kits/LazyComponent";
+import { PageWidth } from "../../../ui-kits/PageWidth";
+import { SectionHeader } from "../../../ui-kits/Sections/SectionHeader/SectionHeader";
+import { SectionWrapper } from "../../../ui-kits/Sections/SectionWrapper/SectionWrapper";
 import "./Style.scss";
 
 export const ERPAbout = () => {
   return (
-    <div className="ERP_About">
-      <h2>Una aplicación para cada necesidad</h2>
+    <div className="ERPAbout">
+      <SectionWrapper>
+        <PageWidth isNarrow>
+          <SectionHeader
+            heading={
+              <div
+                className="Text--highlight"
+                data-aos="zoom-in"
+                data-aos-once="true"
+              >
+                Una aplicación para cada necesidad
+              </div>
+            }
+          />
+          <h2></h2>
+          <div className="Grid">
+            <div
+              className="Grid__Cell   1/2--lap-and-up"
+              data-aos="fade-up-right"
+              data-aos-duration="1500"
+              data-aos-once="true"
+            >
+              <h1 className="Text--highlight">El fin de las</h1>
+              <p className="Text--subdued">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Aperiam, ipsum sapiente aspernatur libero repellat quis
+                consequatur ducimus quam nisi exercitationem omnis earum qui.
+              </p>
+              <h1 className="Text--highlight">El fin de las</h1>
+              <p className="Text--subdued">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Aperiam, ipsum sapiente aspernatur libero repellat quis
+                consequatur ducimus quam nisi exercitationem omnis earum qui.
+              </p>
+            </div>
+            <div
+              className="Grid__Cell   1/2--lap-and-up"
+              data-aos="fade-up-left"
+              data-aos-duration="1500"
+              data-aos-once="true"
+            >
+              <div className="Grid ERPAbout__Composition">
+                {Array.from(Array(6).keys()).map((x: number) => (
+                  <div className="Grid__Cell 1/2--phone 1/3--tablet-and-up 1/3--lap-and-up">
+                    <img
+                      src="https://cdn.shopify.com/shopifycloud/brochure/assets/home/hero/in-hero-product-small-0c64c4c1013fc8ac477cb5ff62bf5b56b2903aec697e6b708b90d3b65a09baf8.jpg"
+                      alt="sksk"
+                      className="ERPAbout__Composition--image"
+                    />
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
+        </PageWidth>
+      </SectionWrapper>
     </div>
   );
 };
--- src/pages/ERP/index.tsx
+++ src/pages/ERP/index.tsx
@@ -1,11 +1,12 @@
 import { ERPAbout } from "../../components/ERP/ERPAbout";
 import { ERPBanner } from "../../components/ERP/ERPBanner";
+import { SiteInfo } from "../../components/SiteInfo";
 
 export const ERP = () => {
   return (
     <>
       <ERPBanner />
-      <ERPAbout/>
+      <ERPAbout />
     </>
   );
 };
